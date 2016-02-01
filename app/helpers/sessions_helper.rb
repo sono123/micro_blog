30daf5e9f6b5890f16915d5f379a514ae5f7aa92
sono123 <steveono@gmail.com>
2016-01-31 23:31:26 -0500

    Remove duplicate log_out method.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,11 +27,6 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
-	def log_out
-		session.delete(:user_id)
-		@current_user = nil
-	end
-
 	# Forgets a persistent session.
 	def forget(user)
 		user.forget
