94c4f5240d48825663c9692dca3ce85269badc66
sono123 <steveono@gmail.com>
2016-02-08 00:03:53 -0500

    Add authorization for user edit and update.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	before_action :logged_in_user, only: [:edit, :update]
-	
+	before_action :correct_user, only: [:edit, :update]
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -22,11 +23,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-  	@user = User.find(params[:id])
+  	# @user = User.find(params[:id])
   end
 
   def update
-  	@user = User.find(params[:id])
+  	# @user = User.find(params[:id])
   	if @user.update_attributes(user_params)
   		flash[:success] = "Profile Updated"
   		redirect_to @user
@@ -47,4 +48,18 @@ class UsersController < ApplicationController
 	  		redirect_to login_url
 	  	end
 	  end
+
+	  def correct_user
+	  	@user = User.find(params[:id])
+	  	redirect_to(root_url) unless current_user?(@user)
+	  end
 end
+
+
+
+
+
+
+
+
+
