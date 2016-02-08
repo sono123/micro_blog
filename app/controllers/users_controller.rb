a5d72213d9712ff4e1ac8e7e80fde47c17e16443
sono123 <steveono@gmail.com>
2016-02-07 23:28:28 -0500

    Protect Edit and Update actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:edit, :update]
+	
 	def show
 		@user = User.find(params[:id])
 	end
@@ -26,7 +28,7 @@ class UsersController < ApplicationController
   def update
   	@user = User.find(params[:id])
   	if @user.update_attributes(user_params)
-  		flash[:succcess] = "Profile Updated"
+  		flash[:success] = "Profile Updated"
   		redirect_to @user
   	else
   		render 'edit'
@@ -39,4 +41,10 @@ class UsersController < ApplicationController
 	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	  end
 
+	  def logged_in_user
+	  	unless logged_in?
+	  		flash[:danger] = "Please Log In."
+	  		redirect_to login_url
+	  	end
+	  end
 end
