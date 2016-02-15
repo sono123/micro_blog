07a56fd43048b58ecc5c314285c8feae32dccf99
sono123 <steveono@gmail.com>
2016-02-14 21:58:56 -0500

    Bug fixing.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'resolv-replace'
+
 class UsersController < ApplicationController
 	before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 	before_action :correct_user, only: [:edit, :update]
