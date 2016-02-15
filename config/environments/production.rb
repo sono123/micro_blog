54bd986dc5e37dec42c56745408d802219541ed1
sono123 <steveono@gmail.com>
2016-02-14 22:05:39 -0500

    Bug fixing.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   host = 'calm-meadow-4895.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address     =>     'smpt.sendgrid.net',
+    :address     =>     'smtp.sendgrid.net',
     :port        =>     '587',
     :authentication =>  :plain,
     :user_name   =>     ENV['SENDGRID_USERNAME'],
