567b133099595fe23e6ee2370c9447bacc34c02c
sono123 <steveono@gmail.com>
2016-02-17 22:29:09 -0500

    Add access control (must be logged in) for microposts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,13 +56,13 @@ class UsersController < ApplicationController
 	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	  end
 
-	  def logged_in_user
-	  	unless logged_in?
-	  		store_location
-	  		flash[:danger] = "Please Log In."
-	  		redirect_to login_url
-	  	end
-	  end
+	  # def logged_in_user
+	  # 	unless logged_in?
+	  # 		store_location
+	  # 		flash[:danger] = "Please Log In."
+	  # 		redirect_to login_url
+	  # 	end
+	  # end
 
 	  def correct_user
 	  	@user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
+  # get 'password_resets/new'
+  # get 'password_resets/edit'
 
   root 'static_pages#home'
 
@@ -17,5 +16,6 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
 
 end
